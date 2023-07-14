02fa512ee58f104c858c3956425623ebaa48824f
Kevin Quispe <kevincarlosqa@gmail.com>
2023-07-14 01:55:18 -0500

    add: Create styled

--- src/components/create-page.jsx
+++ src/components/create-page.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Input from "../ui/Input/Input";
 import Button from "../ui/Button/Button";
+import styled from "@emotion/styled";
+
+const FormEdit = styled.form`
+  padding-top: 2rem;
+  width: 350px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
 
 const CreatePage = () => {
   return (
     <div>
       <h1>Create Product</h1>
+      <FormEdit>
+        <Input label="Name" />
+        <Input label="Price" />
+        <Input label="Description" />
+        <Input label="Category" />
+        <Input label="Picture URL" />
+      </FormEdit>
       <Button>Create</Button>
     </div>
   );
--- src/components/edit-page.jsx
+++ src/components/edit-page.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Button from "../ui/Button/Button";
 import Input from "../ui/Input/Input";
 import styled from "@emotion/styled";
 
 const FormEdit = styled.form`
+  padding-top: 2rem;
   width: 350px;
   margin: auto;
   display: flex;
@@ -14,9 +15,29 @@ const FormEdit = styled.form`
 
 const EditPage = () => {
   const { dish_id } = useParams();
-  const dish = JSON.parse(localStorage.getItem("dishes"));
-  console.log(dish);
+  const dishes = JSON.parse(localStorage.getItem("dishes"));
+  const dish = dishes.filter((filt) => filt.id === +dish_id);
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: dish[0].name,
+    price: dish[0].price,
+    description: dish[0].description,
+    category: dish[0].category,
+    pictureURL: dish[0].picture_url,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes hacer algo con los valores del formulario
+  };
   function handleBack() {
     navigate("/");
   }
@@ -24,11 +45,37 @@ const EditPage = () => {
     <div>
       <h1>Edit Product</h1>
       <FormEdit>
-        <Input label="Name" />
-        <Input label="Price" />
-        <Input label="Description" />
-        <Input label="Category" />
-        <Input label="Picture URL" />
+        <Input
+          label="Name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <Input
+          label="Price"
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+        />
+        <Input
+          label="Description"
+          name="description"
+          type="textarea"
+          value={formData.description}
+          onChange={handleChange}
+        />
+        <Input
+          label="Category"
+          name="category"
+          value={formData.category}
+          onChange={handleChange}
+        />
+        <Input
+          label="Picture URL"
+          name="pictureURL"
+          value={formData.pictureURL}
+          onChange={handleChange}
+        />
       </FormEdit>
       <Button>Save</Button>
     </div>
